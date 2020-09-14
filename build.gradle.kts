a9167d8ba41ae85d193ab645940c2a4207abbf58
marat-gainullin <marat.gainullin.iv@gmail.com>
2020-09-14 17:57:00 +0200

    Gradle builde script has been introduced

---
+++ build.gradle.kts
@@ -0,0 +1,20 @@
+repositories {
+    mavenLocal()
+    mavenCentral()
+}
+
+plugins {
+    id("java")
+}
+
+dependencies {
+    implementation("javax.xml.bind:jaxb-api:2.3.1")
+    compileOnly("com.google.code.findbugs:annotations:3.0.1u2")
+    testImplementation("io.netty:netty-handler:4.1.33.Final")
+    testImplementation("junit:junit:4.12")
+    testImplementation("ru.yandex.qatools.embed:postgresql-embedded:2.10")
+}
+
+tasks.withType<JavaCompile> {
+    options.encoding = "utf-8"
+}
